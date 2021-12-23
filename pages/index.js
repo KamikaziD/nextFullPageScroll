7243c22c69484d32ff6aabe296ac39cdc82b9edc
KamikaziD <druhfus@gmail.com>
2021-12-23 17:43:45 +0200

    linked to id with context and state

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Section from "../components/Section";
+import { createRef } from "react";
 import Link from "next/Link";
 
 import styles from "../styles/styles.module.css";
@@ -6,21 +7,21 @@ import styles from "../styles/styles.module.css";
 export default function Home() {
   return (
     <div>
-      <Section id="use-cases" color="red">
+      <Section id="usecases">
         <div className={styles.container}>
           <h1>Click the #hashtags</h1>
           <div>
             <h5>
-              <Link href="#casino">
+              <Link href="#casino" ref={createRef()}>
                 <a>#casino</a>
               </Link>{" "}
-              <Link href="#passport">
+              <Link href="#passport" ref={createRef()}>
                 <a>#passport</a>
               </Link>{" "}
-              <Link href="#bank">
+              <Link href="#bank" ref={createRef()}>
                 <a>#bank</a>
               </Link>{" "}
-              <Link href="#documents">
+              <Link href="#documents" ref={createRef()}>
                 <a>#documents</a>
               </Link>
             </h5>
@@ -32,29 +33,29 @@ export default function Home() {
           </div>
         </div>
       </Section>
-      <Section id="casino" color="red">
+      <Section id="casino">
         <div className={styles.container1}>
           <h1>Casino</h1>
           <h3>Some useful information</h3>
         </div>
       </Section>
-      <Section id="passport" color="red">
+      <Section id="passport">
         <div className={styles.container2}>
           <h1>Passport</h1>
           <h3>Some useful information</h3>
         </div>
       </Section>
-      <Section id="bank" color="red">
+      <Section id="bank">
         <div className={styles.container3}>
           <h1>Bank</h1>
           <h3>Some useful information</h3>
         </div>
       </Section>
-      <Section id="documents" color="red">
+      <Section id="documents">
         <div className={styles.container4}>
           <h1>Documents</h1>
           <h3>Some useful information</h3>
-          <Link href="#use-cases">
+          <Link href="#usecases" ref={createRef()}>
             <a>TOP</a>
           </Link>
         </div>
